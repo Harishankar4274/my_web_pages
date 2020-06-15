117b59295587e1caaa37b23cc60e924c52091c23
Harishankar Dubey <harishankardubey2911@gmail.com>
2020-06-15 13:25:04 +0530

    image upload

--- index.php
+++ index.php
@@ -15,6 +15,12 @@ echo `ifconfig`;
 </pre>
 </p>
 
+<h2 style="background-color:#bebebe;">Here I want to share something:</h2>
+
+<p style="color:#f8f8f8;">
+<img src="d2nhzgl93ky19v.cloudfront.net/certificate.jpeg" style="width:100%">
+</p>
+
 </body>
 </html>
 
